62e0cca80b934407e47f7867646aaf425f49addf
Jade <deceptivesmurf@gmail.com>
2020-02-01 16:52:15 +1300

    First Commit - Icon Imgages + XCodeTemplate

---
+++ Application/Application/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Application
+//
+//  Created by Jade Myers on 1/02/20.
+//  Copyright © 2020 Jade Myers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
